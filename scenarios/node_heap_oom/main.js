2cf4bfaac5a275eda8808e641fea6d75b2544c5d
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2023-09-04 12:37:32 +0200

    Fix flaky node.js OOM test
    Allocate 5MB every 200ms instead of allocating a huge chunk at the end
    of EXECUTION_TIME_SEC. This is because when allocating a huge chunk,
    sometimes garbage collector is invoked again when calling
    GetAllocationProfile, which retriggers a NearHeapLimit callback, that
    can end aborting if heap extension is not big enough.

--- scenarios/node_heap_oom/main.js
+++ scenarios/node_heap_oom/main.js
@@ -1,11 +1,13 @@
-
+const refs = []
 
 async function foo(size) {
   const n = size / 8
   const x = []
   x.length = n
   for (let i = 0; i < n; i++) { x[i] = Math.random() }
+  refs.push(x)
 }
 
-const durationMs = (process.argv[2] || process.env.EXECUTION_TIME_SEC || 2) * 1000
-setTimeout(() => foo(1024 * 1024 * 50), durationMs)
+const allocSize = 1024 * 1024 * 5
+const allocPeriodMs = 200
+const interval = setInterval(() => foo(allocSize), allocPeriodMs)
