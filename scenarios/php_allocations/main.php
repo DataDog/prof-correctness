5e05631aab6ffd3f66a54cf1d03526b8ad61e358
Florian Engelhardt <florian.engelhardt@datadoghq.com>
2023-11-14 11:38:07 +0100

    fix PHP allocation pattern

--- scenarios/php_allocations/main.php
+++ scenarios/php_allocations/main.php
@@ -2,17 +2,17 @@
 
 function a()
 {
-    str_repeat("a", 1024 * 12000);
+    str_repeat("a", 1024 * 120_000);
 }
 
 function b()
 {
-    str_repeat("a", 1024 * 6000);
+    str_repeat("a", 1024 * 60_000);
 }
 
 function main()
 {
-    $duration = $_ENV["EXECUTION_TIME_SEC"] ?? 10;
+    $duration = $_ENV["EXECUTION_TIME"] ?? 10;
     $end = microtime(true) + $duration;
     while (microtime(true) < $end) {
         $start = microtime(true);
@@ -23,7 +23,7 @@ function main()
         // so we end up doing 10 iterations per second
         $sleep = (0.1 - $elapsed);
         if ($sleep > 0.0) {
-            usleep($sleep * 1_000_000);
+            usleep((int) ($sleep * 1_000_000));
         }
     }
 }
