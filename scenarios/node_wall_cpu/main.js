6a2173daeb250b75dcb8ea47290684ce4d1f6c2a
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2024-01-22 14:27:10 +0000

    Rename node_wall to node_wall_cpu
    and add CPU test.

---
+++ scenarios/node_wall_cpu/main.js
@@ -0,0 +1,65 @@
+const {isMainThread, Worker, workerData} = require('worker_threads');
+const {pbkdf2} = require('crypto')
+const {promisify} = require('util');
+const pbkdf2Async = promisify(pbkdf2)
+
+function a(niter) {
+  let sum = 0;
+  for (let i = 0; i < niter; i++) {
+    sum += i;
+  }
+  return sum
+}
+
+function b(niter) {
+  let sum = 0;
+  for (let i = 0; i < niter; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+async function c() {
+  await pbkdf2Async('secret', 'salt', 200000, 64, 'sha512');
+}
+
+async function foo(nsecs) {
+  await new Promise((resolve) => setTimeout(resolve, 100));
+
+  let done = false;
+  const niter = 100e6
+  setTimeout(() => done = true, nsecs * 1000);
+
+  let bCumulativeTime = 0;
+  let cCumulativeTime = 0;
+
+  while (!done) {
+    a(niter);
+    const t0 = Date.now();
+    b(2*niter);
+    const t1 = Date.now();
+    bCumulativeTime += t1-t0;
+    while (cCumulativeTime < bCumulativeTime) {
+      const t0 = Date.now();
+      await c();
+      const t1 = Date.now();
+      cCumulativeTime += t1-t0;
+    }
+  }
+}
+
+let executionTime = 0
+
+if (isMainThread) {
+  executionTime = process.argv[2] || process.env.EXECUTION_TIME_SEC || 2
+  const nworkers = process.argv[3] || process.env.NWORKERS || 1
+  console.log(`executionTime: ${executionTime}, nworkers: ${nworkers}`)
+  
+  for (let i = 0; i < nworkers; i++) {
+    new Worker(__filename, {workerData: {executionTime: executionTime/2}});
+  }
+} else {
+  executionTime = workerData.executionTime;
+}
+
+foo(executionTime)
