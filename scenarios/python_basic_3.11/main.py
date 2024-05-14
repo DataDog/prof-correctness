8fee2b1309fff68e85d99b43c8a28d8a4e47077d
Tae Gyun Kim <taegyun.kim@datadoghq.com>
2024-05-14 15:37:13 -0400

    add comments
    Signed-off-by: Tae Gyun Kim <taegyun.kim@datadoghq.com>

--- scenarios/python_basic_3.11/main.py
+++ scenarios/python_basic_3.11/main.py
@@ -7,8 +7,11 @@ from ddtrace.profiling import Profiler
 def target(n):
     sleep(n)
 
-
 if __name__ == "__main__":
+    # Simple application that runs for two seconds. Each of the spawned threads
+    # including the ones from the profiler will be attributed one second of
+    # wall time for each second that the application runs. Except for the thread
+    # that explicitly sleeps for only a second.
     prof = Profiler()
     prof.start()  # Should be as early as possible, eg before other imports, to ensure everything is profiled
 
