5d2d20d55ffc113b59e49698084a593ac1717000
r1viollet <erwan.viollet@datadoghq.com>
2025-03-19 18:12:55 +0100

    Java - Add a dockerfile for alpine 3.21.3 and OpenJDK 21

---
+++ scenarios/java_alpine3.21.3_Java21/CpuBurner.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class CpuBurner {
+    public static void main(String[] args) {
+        int numThreads = 2;
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+
+        int executionTimeSec = Integer.parseInt(System.getenv().getOrDefault("EXECUTION_TIME_SEC", "60"));
+
+        System.out.println("Starting CPU intensive tasks on " + numThreads + " threads for " + executionTimeSec + " seconds...");
+
+        for (int i = 0; i < numThreads; i++) {
+            executor.submit(() -> {
+                while (!Thread.currentThread().isInterrupted()) {
+                    double value = Math.pow(Math.random(), Math.random());
+                }
+            });
+        }
+
+        try {
+            Thread.sleep(executionTimeSec * 1000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        executor.shutdownNow();
+        System.out.println("CPU intensive tasks stopped.");
+    }
+}
