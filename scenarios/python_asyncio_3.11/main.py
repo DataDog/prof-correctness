f8876ccdf2f51d88190320c17a6052d04fe4636a
Thomas Kowalski <thomas.kowalski@datadoghq.com>
2025-11-18 11:00:31 +0100

    fixup! [python] Add asyncio check

--- scenarios/python_asyncio_3.11/main.py
+++ scenarios/python_asyncio_3.11/main.py
@@ -1,24 +1,28 @@
 import os
 import asyncio
-from ddtrace.profiling import Profiler
 
 
-async def my_coroutine(n):
+async def my_coroutine(n) -> None:
     await asyncio.sleep(n)
 
 
-async def main():
-    # Simple application that creates two threads with different durations:
-    # - MainThread runs target() for 2 seconds
-    # - Worker Thread-1 runs target() for 1 second
-    # The profiler should capture both threads with their respective durations.
+async def main() -> None:
+    from ddtrace.profiling import Profiler
+
     prof = Profiler()
     prof.start()  # Should be as early as possible, eg before other imports, to ensure everything is profiled
 
+    # Simple application that creates two Tasks with different durations:
+    # - "unnamed Task" runs my_coroutine() for 2 second
+    # - short_task runs my_coroutine() for 1 second
+    # The profiler should capture both threads with their respective durations.
+
     EXECUTION_TIME_SEC = int(os.environ.get("EXECUTION_TIME_SEC", "2"))
 
-    t = asyncio.create_task(my_coroutine(EXECUTION_TIME_SEC / 2))
-    await asyncio.gather(t, my_coroutine(EXECUTION_TIME_SEC))
+    short_task = asyncio.create_task(my_coroutine(EXECUTION_TIME_SEC / 2))
+
+    # asyncio.gather will automatically wrap my_coroutine into a Task
+    await asyncio.gather(short_task, my_coroutine(EXECUTION_TIME_SEC))
 
 
 if __name__ == "__main__":
